650a21613748a831da0286acaabf3242f60d8ee3
Hasanbek Elmurodov <hasanbek.elmurodov@gmail.com>
2021-06-01 18:57:06 +0500

    login form fixed

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -30,7 +30,7 @@ use yii\helpers\Html;
                 <?= $form->field($model, 'rememberMe')->checkbox([
                     'template' => '<div class="icheck-primary">{input}{label}</div>',
                     'labelOptions' => [
-                        'class' => ''
+                        'class' => 'hidden'
                     ],
                     'uncheck' => null
                 ]) ?>
@@ -42,23 +42,6 @@ use yii\helpers\Html;
 
         <?php \yii\bootstrap4\ActiveForm::end(); ?>
 
-        <div class="social-auth-links text-center mb-3">
-            <p>- OR -</p>
-            <a href="#" class="btn btn-block btn-primary">
-                <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-            </a>
-            <a href="#" class="btn btn-block btn-danger">
-                <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-            </a>
-        </div>
-        <!-- /.social-auth-links -->
-
-        <p class="mb-1">
-            <a href="forgot-password.html">I forgot my password</a>
-        </p>
-        <p class="mb-0">
-            <a href="register.html" class="text-center">Register a new membership</a>
-        </p>
     </div>
     <!-- /.login-card-body -->
 </div>
