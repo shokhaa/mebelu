c71e0a32a0045b09f60a71afa29b0499a561b139
Hasanbek Elmurodov <hasanbek.elmurodov@gmail.com>
2021-06-24 15:33:53 +0500

    fixed

--- backend/views/product/_form.php
+++ backend/views/product/_form.php
@@ -15,42 +15,36 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-5">
             <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-
         </div>
 
         <div class="col-md-3">
             <?= $form->field($model, 'price')->textInput(['maxlength' => true]) ?>
         </div>
+
         <div class="col-md-1">
             <?= $form->field($model, 'status')->dropDownList(ProductStatus::getList()) ?>
         </div>
+
         <div class="col-md-2">
             <?= ($model->status == 4)?$form->field($model, 'sales_procent')->textInput(['min'=>0,'max' => 100]):false
             ?>
         </div>
-
-
-
-
-
-            <?= $form->field($model, 'description')->widget(TinyMce::className(),  [
-                'options' => ['rows' => 6],
-                'language' => 'en',
-                'clientOptions' => [
-                    'plugins' => [
-                        "advlist autolink lists link charmap print preview anchor",
-                        "searchreplace visualblocks code fullscreen",
-                        "insertdatetime media table contextmenu paste"
-                    ],
-                    'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
-                ]
-            ]) ?>
     </div>
 
-
-
+    <?= $form->field($model, 'description')->widget(TinyMce::className(),  [
+        'options' => ['rows' => 6],
+        'language' => 'en',
+        'clientOptions' => [
+            'plugins' => [
+                "advlist autolink lists link charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table contextmenu paste"
+            ],
+            'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+        ]
+    ]) ?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
